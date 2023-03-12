1dbe514b94aca31b5a25c82797fa35b60ebbe4a0
alalupo <marcin.sokalski@yahoo.com>
2023-03-12 15:34:12 +0100

    channels last

--- dlgo/networks/large.py
+++ dlgo/networks/large.py
@@ -4,33 +4,34 @@ from keras.layers.convolutional import Conv2D, ZeroPadding2D
 
 
 def layers(input_shape):
+    channels_format = 'channels_last'
     return [
-        ZeroPadding2D((3, 3), input_shape=input_shape, data_format='channels_first'),
-        Conv2D(64, (7, 7), padding='valid', data_format='channels_first'),
+        ZeroPadding2D((3, 3), input_shape=input_shape, data_format=channels_format),
+        Conv2D(64, (7, 7), padding='valid', data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((2, 2), data_format='channels_first'),
-        Conv2D(64, (5, 5), data_format='channels_first'),
+        ZeroPadding2D((2, 2), data_format=channels_format),
+        Conv2D(64, (5, 5), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((2, 2), data_format='channels_first'),
-        Conv2D(64, (5, 5), data_format='channels_first'),
+        ZeroPadding2D((2, 2), data_format=channels_format),
+        Conv2D(64, (5, 5), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((2, 2), data_format='channels_first'),
-        Conv2D(48, (5, 5), data_format='channels_first'),
+        ZeroPadding2D((2, 2), data_format=channels_format),
+        Conv2D(48, (5, 5), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((2, 2), data_format='channels_first'),
-        Conv2D(48, (5, 5), data_format='channels_first'),
+        ZeroPadding2D((2, 2), data_format=channels_format),
+        Conv2D(48, (5, 5), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((2, 2), data_format='channels_first'),
-        Conv2D(32, (5, 5), data_format='channels_first'),
+        ZeroPadding2D((2, 2), data_format=channels_format),
+        Conv2D(32, (5, 5), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((2, 2), data_format='channels_first'),
-        Conv2D(32, (5, 5), data_format='channels_first'),
+        ZeroPadding2D((2, 2), data_format=channels_format),
+        Conv2D(32, (5, 5), data_format=channels_format),
         Activation('relu'),
 
         Flatten(),
--- dlgo/networks/medium.py
+++ dlgo/networks/medium.py
@@ -4,25 +4,26 @@ from keras.layers.convolutional import Conv2D, ZeroPadding2D
 
 
 def layers(input_shape):
+    channels_format = 'channels_last'
     return [
-        ZeroPadding2D((2, 2), input_shape=input_shape, data_format='channels_first'),
-        Conv2D(64, (5, 5), padding='valid', data_format='channels_first'),
+        ZeroPadding2D((2, 2), input_shape=input_shape, data_format=channels_format),
+        Conv2D(64, (5, 5), padding='valid', data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((2, 2), data_format='channels_first'),
-        Conv2D(64, (5, 5), data_format='channels_first'),
+        ZeroPadding2D((2, 2), data_format=channels_format),
+        Conv2D(64, (5, 5), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((1, 1), data_format='channels_first'),
-        Conv2D(64, (3, 3), data_format='channels_first'),
+        ZeroPadding2D((1, 1), data_format=channels_format),
+        Conv2D(64, (3, 3), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((1, 1), data_format='channels_first'),
-        Conv2D(64, (3, 3), data_format='channels_first'),
+        ZeroPadding2D((1, 1), data_format=channels_format),
+        Conv2D(64, (3, 3), data_format=channels_format),
         Activation('relu'),
 
-        ZeroPadding2D((1, 1), data_format='channels_first'),
-        Conv2D(64, (3, 3), data_format='channels_first'),
+        ZeroPadding2D((1, 1), data_format=channels_format),
+        Conv2D(64, (3, 3), data_format=channels_format),
         Activation('relu'),
 
         Flatten(),
